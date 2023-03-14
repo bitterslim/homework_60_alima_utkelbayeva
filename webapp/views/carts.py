ef0921cf63c8cd192aa945b24837a760548ae997
Alima Utkelbayeva <alima150201@gmail.com>
2023-03-14 19:09:49 +0600

    Добавлен метод реализации корзины

--- webapp/views/carts.py
+++ webapp/views/carts.py
@@ -1,20 +1,44 @@
-from django.shortcuts import get_object_or_404
+from django.contrib import messages
+from django.shortcuts import get_object_or_404, redirect
+from django.urls import reverse
+from django.views import View
+from django.views.generic import ListView, DeleteView, CreateView
 
 from webapp.models import Product, Cart
 
 
-def add_basket_view(request, pk):
-    product = get_object_or_404(Product, pk=pk)
-    basket_product = Cart.objects.filter(product=product.pk)
-    if len(basket_product) == 0 and product.leftover != 0:
-        data = {
-            'product': product,
-            'count': 1
-        }
-        Cart.objects.create(**data)
-    elif len(basket_product) != 0:
-          if basket_product[0].count < product.balance:
-            basket_product = get_object_or_404(Basket, pk=basket_product[0].pk)
-            basket_product.count += 1
-            basket_product.save()
-    return redirect('index')
+class CreateCartView(View):
+    def post(self, request, *args, **kwargs):
+        product_id = request.POST.get('product_id')
+        if product_id:
+            try:
+                products = Product.objects.get(id=product_id)
+            except Product.DoesNotExist:
+                messages.error(request, 'This product does not exist.')
+                return products
+            else:
+                cart = request.session.get('cart', {})
+                cart[product_id] = cart.get(product_id, 0) + 1
+                request.session['cart'] = cart
+                messages.success(request, 'Product has been added to your cart.')
+        else:
+            messages.error(request, 'Invalid request.')
+        return redirect(reverse('cart'))
+
+class CartView(ListView):
+    template_name = 'cart.html'
+    model = Cart
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(object_list=object_list, **kwargs)
+        context['product'] = Cart.objects.all()
+        return context
+
+class CartDeleteView(DeleteView):
+    model = Cart
+
+    def post(self, request, *args, **kwargs):
+        product = Cart.objects.filter(pk=kwargs['pk'])
+        product.delete()
+        return redirect('index')
+
