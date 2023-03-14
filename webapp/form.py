e0a582c0340859edc048851a6515b93d8d0cd459
Alima Utkelbayeva <alima150201@gmail.com>
2023-03-14 19:15:43 +0600

    Дабвлена форма создание заказа

--- webapp/form.py
+++ webapp/form.py
@@ -1,7 +1,8 @@
 from django import forms
 from django.core.exceptions import ValidationError
 
-from webapp.models import Product
+from webapp.models import Product, Cart
+from webapp.models.order import Order
 
 
 class ProductForm(forms.ModelForm):
@@ -19,5 +20,11 @@ class ProductForm(forms.ModelForm):
 
 class SearchForm(forms.Form):
     search = forms.CharField(max_length=100, required=False, label='Find')
+
+
+class OrderForm(forms.ModelForm):
+    class Meta:
+        model = Order
+        fields = ('user_name', 'phone', 'address')
 
 
