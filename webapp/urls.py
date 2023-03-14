c8422297a2fbb1e83080f6bedeb9e0f2d703424f
Alima Utkelbayeva <alima150201@gmail.com>
2023-03-14 19:16:50 +0600

    Добавлены ссылки

--- webapp/urls.py
+++ webapp/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 
 from webapp.views.index import IndexView
 from webapp.views.product import ProductDeleteView, ProductDetailView, ProductCreateView, ProductUpdateView
+from webapp.views.carts import CartView, CartDeleteView, CreateCartView
 
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
@@ -9,5 +10,6 @@ urlpatterns = [
     path('products/add', ProductCreateView.as_view(), name='product_add'),
     path('products/<int:pk>/update', ProductUpdateView.as_view(), name='product_update'),
     path('products/<int:pk>/delete', ProductDeleteView.as_view(), name='product_delete'),
-
+    path('products/cart', CartView.as_view(), name='cart'),
+    path('cart/', CreateCartView.as_view(), name='cart_create'),
 ]
